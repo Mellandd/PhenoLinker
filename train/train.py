7c523426290c4f284939ff20406f9c878ad20f79
Jose Luis Mellina Andreu <45824614+Mellandd@users.noreply.github.com>
2025-06-07 11:01:32 +0200

    Update train.py change mps to cuda

--- train/train.py
+++ train/train.py
@@ -22,7 +22,7 @@ data = torch.load('../data/dataframe.pt', weights_only=False)
 data = T.ToUndirected()(data)
 data["phenotype"].x = data["phenotype"].x.to(torch.float32)
 data["gene"].x = data["gene"].x.to(torch.float32)
-device = torch.device('mps' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Generate train, eval, test splits
 transform = T.RandomLinkSplit(
